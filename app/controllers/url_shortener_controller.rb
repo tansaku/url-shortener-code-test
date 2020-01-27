39972ae0b5e403010cbe4ea50ac87118903d14c4
Sam Joseph <tansaku@gmail.com>
2020-01-27 17:11:48 +0000

    ensure that all functionality can be accessed by curl

--- app/controllers/url_shortener_controller.rb
+++ app/controllers/url_shortener_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class UrlShortenerController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:index]
+
   def index
     render json: { "short_url": "/abc123", "url": "http://www.farmdrop.com" }
   end
 
   def redirect
-    redirect_to "http://www.farmdrop.com"
+    redirect_to "http://www.farmdrop.com", status: 301
   end
 end
--- spec/url_shortener_api_spec.rb
+++ spec/url_shortener_api_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-describe "post /", type: "request" do
+describe "Url Shortener", type: "request" do
   let(:valid_params) do
     {
       url: "http://www.farmdrop.com"
@@ -13,13 +13,29 @@ describe "post /", type: "request" do
     '{"short_url":"/abc123","url":"http://www.farmdrop.com"}'
   end
 
-  it "should shorten urls" do
-    post "/", params: valid_params
-    expect(response.body).to eq expected_response
+  context "POST /" do
+    # this around block ensures that the system supports using a simple curl operation
+    # as requested in the specification, as this ensures the tests will fail
+    #  if our rails controller does not skip the verify_authenticity_token checks
+    # that usually ensure that we are submitting from a rails form
+    # see https://stackoverflow.com/a/28947362/316729
+    around do |example|
+      ActionController::Base.allow_forgery_protection = true
+      example.run
+      ActionController::Base.allow_forgery_protection = false
+    end
+
+    it "should shorten urls" do
+      post "/", params: valid_params
+      expect(response.body).to eq expected_response
+    end
   end
 
-  it "should redirect to original URL" do
-    get "/abc123"
-    expect(response).to redirect_to "http://www.farmdrop.com"
+  context "GET" do
+    it "should redirect to original URL" do
+      get "/abc123"
+      expect(response).to redirect_to "http://www.farmdrop.com"
+      expect(response.status).to eq 301
+    end
   end
 end
